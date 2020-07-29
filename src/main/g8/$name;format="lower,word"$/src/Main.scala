9e91675380b9ff8e86aae37fafa34850b0ff0cf1
Kristofer Ralph <kris.ralph45@gmail.com>
2020-07-29 19:15:11 +0100

    fixup test

--- src/main/g8/$name;format="lower,word"$/src/Main.scala
+++ src/main/g8/$name;format="lower,word"$/src/Main.scala
@@ -3,6 +3,6 @@ package $name;format="lower,word"$
 // use as a tester that your mill build tool is working.
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello $name$");
+    println("Hello $name$")
   }
 }
--- src/main/g8/$name;format="lower,word"$/test/src/MainSpec.scala
+++ src/main/g8/$name;format="lower,word"$/test/src/MainSpec.scala
@@ -1,17 +1,16 @@
-package $name;format="lower,word"$.test
+package $name;format="lower,word"$
 
-import collection.mutable.Stack
 import org.scalatest._
 import flatspec._
 import matchers._
+import java.io.ByteArrayOutputStream
 
-// mercilessly ripped from scalatest docs just to test it's working
 class ExampleSpec extends AnyFlatSpec with should.Matchers {
   "Main.main()" should "print Hello World" in {
     val out = new ByteArrayOutputStream()
     Console.withOut(out) {
-      Main.main(List())
+      Main.main(Array())
     }
-    out.toString should be ("Hello World\n")
+    out.toString should be ("Hello $name$\n")
   }
 }
